6f010f9c744b1ee80705145b3bb9fceaa9471c92
unknown <dkmeade@gmail.com>
2018-09-11 15:26:32 -0600

    adding final finals

---
+++ app/data/friends.js
@@ -0,0 +1,45 @@
+var friendsArray = [
+    {
+    "name": "Harvey Specter",
+    "photo": "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=imgres&cd=&cad=rja&uact=8&ved=2ahUKEwjJkbCr5bHdAhXm5YMKHTmJBW8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.quora.com%2FWho-is-Harvey-Specter&psig=AOvVaw0U8qZTJeudhGIRdQi-GOnX&ust=1536715462001146",
+    "scores": [5, 3, 5, 5, 5, 1, 5, 4, 5, 5]
+}, {
+    "name": "Mike Ross",
+    "photo": "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi26qP15bHdAhWJzIMKHbTtAvsQjRx6BAgBEAU&url=http%3A%2F%2Fwww.usanetwork.com%2Fsuits%2Fphotos%2Fmike-ross&psig=AOvVaw0rhhSwMCRBrBdLEjo5F6bP&ust=1536715616397972",
+    "scores": [5, 1, 3, 4, 5, 5, 1, 4, 1, 3]
+}, {
+    "name": "Rachel Zane",
+    "photo": "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjD3byU5rHdAhWEy4MKHf6MBoIQjRx6BAgBEAU&url=http%3A%2F%2Fwww.usanetwork.com%2Fsuits%2Fcast%2Frachel-zane&psig=AOvVaw0-oPAteNeUDXpcpFcZBj_7&ust=1536715681673684",
+    "scores": [1, 5, 3, 4, 3, 4, 2, 2, 4, 1]
+}, {
+    "name": "Louis Litt",
+    "photo": "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=imgres&cd=&cad=rja&uact=8&ved=2ahUKEwjM2L6t5rHdAhWW0YMKHVnjAMAQjRx6BAgBEAU&url=http%3A%2F%2Fsuits.wikia.com%2Fwiki%2FFile%3ALouis_Litt_-_USA_Network_Promotional_Photo_(2).jpg&psig=AOvVaw2e7W-7LwVjLnRVPniEbWoU&ust=1536715734853038",
+    "scores": [5, 1, 2, 5, 3, 4, 1, 2, 5, 1]
+}, {
+    "name": "Donna Paulsen",
+    "photo": "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=imgres&cd=&cad=rja&uact=8&ved=2ahUKEwit5svR5rHdAhUr4YMKHbswCmgQjRx6BAgBEAU&url=http%3A%2F%2Fwww.usanetwork.com%2Fsuits%2Fcast%2Fdonna-paulsen&psig=AOvVaw11SG77H-yRcGhllZtIVdol&ust=1536715810635031",
+    "scores": [1, 5, 3, 4, 2, 1, 5, 5, 5, 5]
+}, {
+    "name": "Jessica Pearson",
+    "photo": "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwih2pz65rHdAhUk5oMKHZR9Bk0QjRx6BAgBEAU&url=https%3A%2F%2Fblackandquirkyviews.com%2Ftag%2Fjessica-pearson%2F&psig=AOvVaw2xEgnl2Yitu67BbT1O4isc&ust=1536715885009218",
+    "scores": [1, 5, 3, 4, 2, 1, 5, 5, 5, 5]
+}, {
+    "name": "Katrina Bennett",
+    "photo": "https://i.pinimg.com/originals/1a/ea/81/1aea81b415084f7c699f090702d0c34e.jpg",
+    "scores": [3, 3, 3, 3, 3, 2, 5, 3, 2, 5]
+}, {
+    "name": "Daniel Hardman",
+    "photo": "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=imgres&cd=&cad=rja&uact=8&ved=2ahUKEwiSi8PI57HdAhVK0YMKHeC0Dk8QjRx6BAgBEAU&url=http%3A%2F%2Fsuits.wikia.com%2Fwiki%2FDaniel_Hardman&psig=AOvVaw3M8-i2euzVELxwi_AWtOa1&ust=1536716059958125",
+    "scores": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+}, {
+    "name": "Robert Zane",
+    "photo": "https://vignette.wikia.nocookie.net/suits/images/9/98/Robert_Zane_%287x09%29.png/revision/latest?cb=20170908105246",
+    "scores": [5, 2, 3, 5, 2, 5, 5, 4, 4, 5]
+}, {
+    "name": "Travis Tanner",
+    "photo": "https://vignette.wikia.nocookie.net/suits/images/7/7d/Travis_Tanner_%282x04%29.png/revision/latest?cb=20170813171252",
+    "scores": [1, 1, 2, 5, 4, 1, 4, 3, 5, 1]
+}
+];
+
+module.exports = friendsArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,101 @@
+var friendsData = require("../data/friends");
+
+module.exports = function(app) {
+    app.get("/api/friends", function(req, res) {
+        return res.json(friendsData);
+    });
+
+    app.get("/api/friends", function(req, res) {
+        res.sendFile(Path2D.join(__dirname, "/api/friends"));
+    });
+
+    app.post("/api/friends", function(req, res) {
+        var data = req.body;
+        var friendName = req.body.name;
+        var friendPhoto = req.body.photo;
+        var friendScores = data.scores;
+
+        var match = {
+            name: "",
+            photo: ""   
+        };
+
+        var bestDifference = Infinity 
+
+        var scoreDiff;
+
+        for (var i = 0; i < friendsData.length; i++) {
+            var checkFriend = friendsData[i];
+            newDiff = 0;
+            console.log(checkFriend);
+            for (var j = 0; j < checkFriend.scores.length; j++) {
+                var checkFriendAnswers = checkFriend.scores[j];
+                var currentUserAnswers = friendScores[j];
+                newDiff += Math.abs(parseInt(currentUserAnswers) - parseInt(checkFriendAnswers));
+                console.log(newDiff)
+            }
+            if (newDiff <= bestDifference) {
+                match = checkFriend;
+                // console.log("================= ", match);
+
+            }
+        }
+
+        console.log(match)
+        friendsData.push(data);
+        res.json(match);
+
+
+    })
+}
+
+
+
+// module.exports = function(app) {
+// app.get("/api/friends", function(req, res) {
+//     res.json(friendsData);
+// });
+
+// app.post("/api/friends", function(req, res) {
+//     var newFriend = req.body;
+//     // console.log(newFriend);
+
+//     var bestMatch = {};
+
+//     for (var i = 0; i <newFriend.answers.length; i++) {
+//         if(newFriend.answers[i] == "1 (Strongly Disagree)") {
+//             newFriend.answers[i] = 1;
+//         } else if (newFriend.answers[i] == "5 (Strongly Agree)") {
+//             newFriend.answers[i] = 5;
+//         } else {
+//             newFriend.answers[i] = parseInt(newFriend.answers[i]);
+//         }
+//     }
+
+//     var bestMatchIndex = 0;
+
+//     var bestMatchDifference = 40;
+
+//     for (var i = 0; i < friendsData.length; i++) {
+//         var totalDifference = 0;
+
+//         for (var index = 0; index < friendsData[i].answers.length; index++) {
+//             var differenceInData = Math.abs(friendsData[i].answers[index] - newFriend.answers[index]);
+//             totalDifference += differenceInData;
+//         }
+
+//         if (totalDifference < bestMatchDifference) {
+//             bestMatchIndex = i;
+//             bestMatchDifference = totalDifference;
+//         }
+
+//         bestMatch = friendsData[bestMatchIndex];
+
+//         friendsData.push(newFriend);
+
+//         res.json(bestMatch);
+//     }
+// })
+
+
+// };
